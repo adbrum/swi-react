a5ceec32e5e82a605249afb9702ba390a88e277f
Adriano Regis <adbrum@outlook.com>
2021-08-18 14:47:07 +0100

    Showing Input Errors

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -8,15 +11,34 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data): void {
-    console.log(data);
-  }
+  const formRef = useRef<FormHandles>(null);
+  console.log(formRef);
+
+  const handleSubmit = useCallback(async data => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Required name'),
+        email: Yup.string()
+          .email('Enter a valid email address')
+          .required('Required email'),
+        password: Yup.string().min(6, 'at least 6 digits'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
 
   return (
     <Container>
       <Background />
       <Content>
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Create account</h1>
 
           <Input name="name" icon={FiUser} placeholder="Name" />
