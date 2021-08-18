c4dca1a64a7dfdf43a733aec5bdd9b6407fb00e0
Adriano Regis <adbrum@outlook.com>
2021-08-18 11:21:53 +0100

    Isolating components

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,36 @@
+import styled, { css } from 'styled-components';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+}
+
+export const Container = styled.div`
+  background: #232129;
+  border-radius: 10px;
+  border: 2px solid #232129;
+  padding: 16px;
+  width: 100%;
+  color: #666360;
+  display: flex;
+  align-items: center;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  input {
+    flex: 1;
+    background: transparent;
+    border: 0;
+    color: #f4ede8;
+    &::placeholder {
+      color: #666360;
+    }
+  }
+
+  svg {
+    margin-right: 16px;
+  }
+`;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
@@ -9,11 +12,16 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Login</h1>
 
-        <input placeholder="E-mail" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-        <input type="password" placeholder="Password" />
+        <Input
+          name="password"
+          type="password"
+          icon={FiLock}
+          placeholder="Password"
+        />
 
-        <button type="submit">Enter</button>
+        <Button type="submit">Enter</Button>
         <a href="forgot">Forgot my password</a>
       </form>
       <a href="login">
