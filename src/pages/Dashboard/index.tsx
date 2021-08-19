3b404c87d9d4d116a64736f72d61c78badf67cef
Adriano Regis <adbrum@outlook.com>
2021-08-19 18:19:26 +0100

    Added button signOut

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
+import { useAuth } from '../../hooks/auth';
 
 // import { Container } from './styles';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+const Dashboard: React.FC = () => {
+  const { signOut, key } = useAuth();
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <button type="button" onClick={signOut}>
+        <FiPower />
+        Exit
+      </button>
+    </>
+  );
+};
 
 export default Dashboard;
